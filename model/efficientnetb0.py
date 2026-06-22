d8ec9490b9b4d78fb55f86b9068a3fd6dc5bcc77
Aditi Goyal <143211287+aditiigoyall@users.noreply.github.com>
2026-06-22 11:39:35 +0530

    Create efficientnetb0.py

---
+++ model/efficientnetb0.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.applications import EfficientNetB0
+
+IMG_SIZE = 224
+NUM_CLASSES = 2
+
+def build_model(num_classes=NUM_CLASSES):
+
+    inputs = layers.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
+
+    base_model = EfficientNetB0(
+        include_top=False,
+        weights="imagenet",
+        input_shape=(IMG_SIZE, IMG_SIZE, 3)
+    )
+
+    base_model.trainable = False
+
+    x = base_model(inputs, training=False)
+    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Dropout(0.2)(x)
+
+    outputs = layers.Dense(
+        num_classes,
+        activation="softmax"
+    )(x)
+
+    model = tf.keras.Model(inputs, outputs)
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
